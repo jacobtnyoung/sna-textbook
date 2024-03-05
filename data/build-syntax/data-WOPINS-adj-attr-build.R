f9c1f3dc6bb6ad8fb3d91a23362b923878e52005
jacobtnyoung <CRJ605@asu.edu>
2024-03-05 12:18:55 -0700

    additions to WOPINS trust data

--- data/build-syntax/data-WOPINS-adj-attr-build.R
+++ data/build-syntax/data-WOPINS-adj-attr-build.R
@@ -75,6 +75,82 @@ write.attributes( SDD.net   , c( "Age", "Race" ), "data-WOPINS-s1gb-age-race-att
 write.attributes( SDD.gb.net, c( "Age", "Race" ), "data-WOPINS-s2gb-age-race-attributes" )
 write.attributes( SDD.gp.net, c( "Age", "Race" ), "data-WOPINS-s2gp-age-race-attributes" )
 
+write.attributes( SDD.net   , c( "White", "YearsOnUnit" ), "data-WOPINS-s1gb-white-you-attributes" )
+write.attributes( SDD.gb.net, c( "White", "YearsOnUnit" ), "data-WOPINS-s2gb-white-you-attributes" )
+write.attributes( SDD.gp.net, c( "White", "YearsOnUnit" ), "data-WOPINS-s2gp-white-you-attributes" )
+
+
+# ----
+# create the network objects as .rds files with the attributes
+
+
+# create a function to run over each of the networks
+
+build.network <- function( networkName, filePath, netFileName, attrPathName ){
+  
+  # define the network
+  networkName <- as.network( 
+    as.matrix( 
+      read.csv( 
+        paste( filePath, netFileName, sep = "" ),
+        as.is = TRUE,
+        header = TRUE,
+        row.names = 1 
+      ) 
+    ),
+    directed = TRUE 
+  )
+  
+  # read the attributes in
+  attrDat <- read.csv( 
+    paste( filePath, attrPathName, sep = "" ),
+    as.is = TRUE,
+    header = TRUE
+  )
+  
+  # create the names for the loop
+  namesLoop <- names( attrDat )
+  
+  # loop through and add the attributes
+  for( i in 1: length( namesLoop ) ){
+    networkName %v% namesLoop[i] <- as.numeric( attrDat[, i ] ) 
+  }
+
+return( networkName )  
+  
+}
+
+trustNetS1 <- build.network(
+  networkName = trustNetS1,
+  filePath = "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/sna-textbook/data/",
+  netFileName = "data-WOPINS-trust-s1gb-adj.csv",
+  attrPathName = "data-WOPINS-s1gb-white-you-attributes.csv"
+)
+
+trustNetS2gb <- build.network(
+  networkName = trustNetS2gb,
+  filePath = "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/sna-textbook/data/",
+  netFileName = "data-WOPINS-trust-s2gb-adj.csv",
+  attrPathName = "data-WOPINS-s2gb-white-you-attributes.csv"
+)
+
+trustNetS2gp <- build.network(
+  networkName = trustNetS2gp,
+  filePath = "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/sna-textbook/data/",
+  netFileName = "data-WOPINS-trust-s2gp-adj.csv",
+  attrPathName = "data-WOPINS-s2gp-white-you-attributes.csv"
+)
+
+
+# ----
+# save the object as an .rds file
+
+path <- "/Users/jyoung20/Dropbox (ASU)/GitHub_repos/sna-textbook/data/"
+
+saveRDS( trustNetS1,   file = paste( path, "data-WOPINS-s1-trust-net",   ".rds", sep = "" ) )
+saveRDS( trustNetS2gb, file = paste( path, "data-WOPINS-s2gb-trust-net", ".rds", sep = "" ) )
+saveRDS( trustNetS2gp, file = paste( path, "data-WOPINS-s2gp-trust-net", ".rds", sep = "" ) )
+
 
 # =============================================================================== #
 # END FILE.
